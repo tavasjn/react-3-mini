d75a66ae2d50c5c8d770a95c8e02d3652c8b39e9
Tavas Neuwirth <tavasjn_24@hotmail.com>
2019-09-26 13:34:34 -0600

    complete

--- src/App.js
+++ src/App.js
@@ -29,8 +29,17 @@ class App extends Component {
   }
 
   getVehicles() {
-    // axios (GET)
-    // setState with response -> vehiclesToDisplay
+    axios.get('https://joes-autos.herokuapp.com/api/vehicles')
+    .then(response => {
+      this.setState({
+        vehiclesToDisplay: response.data
+      })
+      toast.success('Success!')
+    })
+    .catch(error => {
+      console.log(error)
+      toast.error('Failure')
+    })
   }
 
   getPotentialBuyers() {
@@ -41,6 +50,17 @@ class App extends Component {
   sellCar(id) {
     // axios (DELETE)
     // setState with response -> vehiclesToDisplay
+    axios.delete(`https://joes-autos.herokuapp.com/api/vehicles/${id}`)
+    .then(response => {
+      this.setState({
+        vehiclesToDisplay: response.data.vehicles
+      })
+      toast.success('Success On Deleting!')
+    })
+    .catch(err => {
+      console.log(err)
+      toast.error('Delete Failed!')
+    })
   }
 
   filterByMake() {
@@ -60,6 +80,17 @@ class App extends Component {
   updatePrice(priceChange, id) {
     // axios (PUT)
     // setState with response -> vehiclesToDisplay
+    axios.put(`https://joes-autos.herokuapp.com/api/vehicles/${id}/${priceChange}`)
+    .then(response => {
+      this.setState({
+        vehiclesToDisplay: response.data.vehicles
+      })
+      toast.success('Successfully Updated!')
+    })
+    .catch(err => {
+      console.log(err)
+      toast.error('Failed Update!')
+    })
   }
 
   addCar() {
@@ -73,6 +104,17 @@ class App extends Component {
 
     // axios (POST)
     // setState with response -> vehiclesToDisplay
+    axios.post('https://joes-autos.herokuapp.com/api/vehicles', newCar)
+    .then(response => {
+      this.setState({
+        vehiclesToDisplay: response.data.vehicles
+      })
+      toast.success('Successfully Added!')
+    })
+    .catch(err => {
+      console.log(err);
+      toast.error('Failed to Add!')
+    })
   }
 
   addBuyer() {
